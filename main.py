b9d128a0b26d6906192d6ac435ce11505126cb04
echoedinvoker <echoedinvoker@gmail.com>
2025-10-20 06:38:10 +0000

    feat: cicd

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World! -v2"
+    return "Hello, World! -v1 from CICD pipeline"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
